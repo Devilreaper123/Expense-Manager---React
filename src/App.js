479dc207c3e222b89c8c3e3e536a3e9d36a6417e
Devilreaper123 <ronitshahu@gmail.com>
2022-07-27 17:22:05 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Lets Get started</h2>
+      <ExpenseItem> </ExpenseItem>
+      <ExpenseItem> </ExpenseItem>
+      <ExpenseItem> </ExpenseItem>
+      <ExpenseItem> </ExpenseItem>
+      
+    </div>
+  );
+}
+export default App;
