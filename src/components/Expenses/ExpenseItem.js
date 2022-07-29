e5c415a6dfae417a2ffe2b650af8b46faeffe8e2
Devilreaper123 <ronitshahu@gmail.com>
2022-07-29 17:18:40 +0530

    Added some features

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,25 +2,27 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import React, { useState } from "react";
-
+import "../NewExpense/ExpenseForm.css";
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
   const [amount, setAmount] = useState(props.amount);
   const [date, setDate] = useState(props.date);
   console.log("ExpenseItem evaluated by React");
   const clickHandlerTitle = () => {
-    setTitle("Updated!!");
+    setTitle("Updated Title!!");
     console.log({ title });
   };
   const clickHandlerAmount = () => {
     setAmount(299);
+    console.log({ amount });
   };
   const clickHandlerDate = () => {
     setDate(new Date(2000, 1, 22));
+    console.log({ date });
   };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />{" "}
+      <ExpenseDate date={date} />
       <button onClick={clickHandlerDate}>Change Date</button>
       <div className="expense-item__description">
         <h2>{title}</h2>
