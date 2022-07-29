ba7f9cc8fff5e8ce0958df3765916fc34628a3ba
Devilreaper123 <ronitshahu@gmail.com>
2022-07-29 14:15:07 +0530

    added Change Date Button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,20 +6,25 @@ import React, { useState } from "react";
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
   const [amount, setAmount] = useState(props.amount);
+  const [date, setDate] = useState(props.date);
+
   const clickHandlerTitle = () => {
     setTitle("Updated!!");
-    // console.log({ title });
+    console.log({ title });
   };
-  const clickHandler = () => {
+  const clickHandlerAmount = () => {
     setAmount(299);
+  };
+  const clickHandlerDate = () => {
+    setDate(new Date(2000, 1, 22));
   };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} /> <button onClick={clickHandlerDate}>Change Date</button>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
-        <button onClick={clickHandler}>Change Amount</button>
+        <button onClick={clickHandlerAmount}>Change Amount</button>
       </div>
       <button onClick={clickHandlerTitle}>Change title</button>
     </Card>
